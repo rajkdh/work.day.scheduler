487f9403f92aa9336b6e9c85d46c986e4e138580
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-10 16:44:36 -0400

    added current date function to page, displayed in header

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,20 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
+
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd, MMM DD, YYYY');
+  $('#currentDay').text(currentDate)
 });
