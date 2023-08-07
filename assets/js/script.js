29fdd62d840a6f9a93644fb3795c3c8fba027e5c
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-08-07 14:25:26 -0400

    fixed bug. event saves to each respective time block without displaying repeats.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,38 +7,61 @@ $(function () {
 //saves event to local storage. key=event, value=timeblock id       
   $('.saveBtn').on('click', function() {
         
-            var event = $('.description').val();
+            var event = $(this).siblings('.description').val();
             var time = $(this).parent().attr('id');
             localStorage.setItem(time, event);
+            console.log(event);
 
         });
 //time tracked and respected time blocks change accordingly.
+function update() {
+
 var timeBlock = $('.time-block');
 var currentTime = dayjs().hour();
 
-timeBlock.each(function(){
-    // var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-    
-    // if(blockHour === currentTime) {
-    //     $(this).addClass('present');
-    //     $(this).removeClass('past');
-    //     $(this).removeClass('future');
-    // } else if(blockHour < currentTime) {
-    //     $(this).addClass('past');
-    //     $(this).removeClass('present');
-    //     $(this).removeClass('future');
-    // } else {
-    //     $(this).addClass('future');
-    //     $(this).removeClass('past');
-    //     $(this).removeClass('present');
-    // }
+timeBlock.each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
+    if (blockHour < currentTime) {
+
+        $(this).addClass('past');
+        $(this).removeClass('present');
+        $(this).removeClass('future');
+
+
+    } else if (blockHour === currentTime) {
+
+        $(this).removeClass('past');
+        $(this).addClass('present');
+
+    } else {
+
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+
+    }
 
 
 });
 
-//get item from local storage
-// $('.description').val(savedEvent('.time-block'))
+}
+
+
+update();
+
+setInterval(update(), 15000);
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
 
 });
 
